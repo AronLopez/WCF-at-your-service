c71c9d17ae02142456b1d1927bbfc3ad98ca3941
Aron Lopez <avasquez@nearsoft.com>
2013-08-28 17:31:15 -0700

    Nuevo proyecto

---
+++ SimpleServiceSample/SimpleService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServiceSample
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string first;
+        [DataMember]
+        public string last;
+    }
+
+    [ServiceContract]
+    public interface ISimpleService
+    {
+        [OperationContract]
+        string SayHello(Name Person);
+    }
+
+    class SimpleService : ISimpleService
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hi {0} {1}", person.first, person.last);
+        }
+    }
+}
